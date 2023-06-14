980393dd904032e2a8ea42c9a77830afc3bfa333
tod619 <trevorodwyer619@hotmail.com>
2023-06-14 21:06:19 +0100

    Create question_bank and add questions to it in main.py file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # A quiz game using OOP
 # 13/06/2023
+
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(text=question_text, answer=question_answer)
+    question_bank.append(new_question)
